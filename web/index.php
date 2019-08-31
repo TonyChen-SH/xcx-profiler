fbe8ef6356fb97e05f5e96ea5b3fbb217d149c38
Tony Chen <chendan316@gmail.com>
2019-08-31 18:19:39 +0800

    feat: 加入基本的网页扫描.

--- web/index.php
+++ web/index.php
@@ -7,14 +7,5 @@ use XcxProfiler\Bootstrap;
 
 include __DIR__ . '/../vendor/autoload.php';
 
-
-(new Bootstrap())->showAllNotRefImage('/Users/chendan/Project/work-jooj/baodao_member_plus');
-
-// PHP 实时的输出结果
-// echo str_repeat(' ', 1024);
-// for ($i = 0; $i < 10; $i++) {
-//     echo $i . "<br>";
-//     ob_flush();
-//     flush();
-//     sleep(1);
-// }
+$scanPath = $_GET['scanPath'];
+(new Bootstrap())->showAllNotRefImage($scanPath);
